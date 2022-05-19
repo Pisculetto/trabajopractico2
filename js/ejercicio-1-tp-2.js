0cb6a59af84785c0e996b045f0f93a2c6b945e18
Hector Norry <piscudelag@gmail.com>
2022-05-19 06:44:43 -0300

    Se agrega punto 1

---
+++ js/ejercicio-1-tp-2.js
@@ -0,0 +1,14 @@
+let edad = parseInt(prompt('Ingrese su edad en numeros: '));
+
+if(edad >= 18){
+    document.write("Ya puedes conducir yay!");
+}
+
+else
+
+document.write("Sos pibe todavía.");
+
+if (edad === Number){
+    document.write("Ingrese un numero.")
+}
+
