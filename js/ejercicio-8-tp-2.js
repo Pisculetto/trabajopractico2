718c6ade0ec6501359dc421b954c8a71b47d5919
Hector Norry <piscudelag@gmail.com>
2022-05-24 08:36:14 -0300

    Se agrega punto 8

---
+++ js/ejercicio-8-tp-2.js
@@ -0,0 +1,23 @@
+
+let numrep = prompt("Introduce numero de repeticiones");
+
+if (Number(numrep) == numrep) {
+   
+    if (numrep > 0 && numrep <= 50) {
+        let rep,i;
+        
+        for (i = 0; i <=numrep; i++) {
+            
+            for (rep = 1; rep <= i; rep++) {
+                document.write(rep);
+            }
+            document.write("<br>");
+        }
+    } 
+    else {
+        alert("El número introducido no es válido");
+    }
+} 
+else {
+    alert("No has introducido un número");
+}
