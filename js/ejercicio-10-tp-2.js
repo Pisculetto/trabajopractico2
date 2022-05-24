b42251825bdce8d60861d8f7be3036a1141df02a
Hector Norry <piscudelag@gmail.com>
2022-05-24 08:46:07 -0300

    Se agrega el punto 10

---
+++ js/ejercicio-10-tp-2.js
@@ -0,0 +1,29 @@
+let filas = Number(prompt("Introduce numero de filas"));
+
+let columnas = Number(prompt("Introduce numero de columnas"));
+
+let i,j;
+
+let resultado = filas * columnas;
+
+document.write("<table border>");
+
+for (i = 0; i < filas; i++) {
+    
+    document.write("<tr>");
+    
+    for (j = 0; j < columnas; j++) {
+        
+        document.write("<td>");
+       
+        document.write(resultado);
+        
+        resultado--;
+        
+        document.write("</td>");
+    }
+    
+    document.write("</tr>");
+}
+
+document.write("</table>");
